143da24ad58b594ac55c0ae35b7613f46f386f65
jeffperezfrade <alu0101038520@ull.edu.es>
2022-03-22 13:12:37 +0000

    Ejercicio 3

--- src/ejercicio-3/Cifrado.ts
+++ src/ejercicio-3/Cifrado.ts
@@ -1,7 +1,8 @@
-import { Alfabeto } from "./Alfabeto";
-import { Clave } from "./Clave";
+import {Alfabeto} from "./Alfabeto";
+import {Clave} from "./Clave";
 // Paquete requerido para utilizar el prompt.
 const {prompt} = require('enquirer');
+const inquirer = require('inquirer');
 
 export class Cifrado {
     private clave: Clave;
@@ -35,42 +36,90 @@ export class Cifrado {
         return finalString;
     }
 
-    // public decode(entryWord: string): string {hola
-    //     const entrySplitted: string[] = entryWord.split('');
-    //     const alphabetSize: number = this.alfabeto.getAlfabeto().length;
-    //     let finalString: string = '';
-    //     for(let index: number = 0; index < entrySplitted.length; index++){
-    //         if(this.alfabeto.getIndexOf(entrySplitted[index]) != -1 && this.alfabeto.getIndexOf(entrySplitted[index]) >= this.d) {
-    //             finalString += this.alfabeto.getCharAt((this.alfabeto.getIndexOf(entrySplitted[index]) - this.d) % alphabetSize);
-    //         } else if(this.alfabeto.getIndexOf(entrySplitted[index]) != -1 && this.alfabeto.getIndexOf(entrySplitted[index]) < this.d) {
-    //             finalString += this.alfabeto.getCharAt(alphabetSize + (this.alfabeto.getIndexOf(entrySplitted[index]) - this.d));
-    //         } else finalString += entrySplitted[index];
-    //     }        
-    //     return finalString;
-    // }
+    public decode(): string {
+        let finalString: string = '';
+        let fullClave: string = this.clave.getClave();
+        if(this.mensajeEntrada.length > this.clave.getClaveLength()) {
+            let repetir: number = Math.floor(this.mensajeEntrada.length / this.clave.getClaveLength());
+            let claveAmpliada: string = this.clave.getClave().repeat(repetir);
+            let resto: number = this.mensajeEntrada.length - claveAmpliada.length;
+            for(let index: number = 0; index < resto; index++)
+                claveAmpliada += this.clave.getCharAt(index);
+            fullClave = claveAmpliada;
+        }
+
+        for(let index: number = 0; index < this.mensajeEntrada.length; index++) {
+            let d: number = this.alfabeto.getIndexOf(fullClave[index]) + 1;
+            if(this.alfabeto.getIndexOf(this.mensajeEntrada[index]) != -1 && this.alfabeto.getIndexOf(this.mensajeEntrada[index]) >= d){
+                let pos: number = (this.alfabeto.getIndexOf(this.mensajeEntrada[index]) - d) % this.alfabeto.getLength();
+                finalString += this.alfabeto.getCharAt(pos);
+            } else if(this.alfabeto.getIndexOf(this.mensajeEntrada[index]) != -1 && this.alfabeto.getIndexOf(this.mensajeEntrada[index]) < d) {
+                finalString += this.alfabeto.getCharAt(this.alfabeto.getLength() + (this.alfabeto.getIndexOf(this.mensajeEntrada[index]) - d));
+            } else finalString += this.mensajeEntrada[index];
+        }
+        return finalString;
+    }
 }
 
 const alfabeto: string = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
 
 const start = async function() {
-    console.log("\nBienvenido al programa de Cifrado César.");
-    const response = await prompt([
-        {
-            type: 'input',
-            name: 'mensajeEntrada',
-            message: 'Mensaje de entrada:',
-        },
-        {
-            type: 'input',
-            name: 'clave',
-            message: 'Palabra Clave:',
-        },
-    ]);
-    const cifradoCesar = new Cifrado(response.clave, response.mensajeEntrada, alfabeto);
-    console.log(`\nEl cifrado se realizará con el alfabeto: ${alfabeto}`);
-
-    console.log(cifradoCesar.encode());
-
-}
+    let stop: boolean = false;
+    while(!stop){
+        const res = inquirer.prompt([
+            {
+              type: 'list',
+              name: 'eleccion',
+              message: '¿Que quieres hacer?:',
+              choices: ['Codificar', 'Decodificar', 'Salir'],
+            },
+          ])
+          .then(async (answers) => {
+            if(answers.eleccion == "Codificar"){
+                    const response = await prompt([
+                {
+                    type: 'input',
+                    name: 'mensajeEntrada',
+                    message: 'Mensaje de entrada:',
+                },
+                {
+                    type: 'input',
+                    name: 'clave',
+                    message: 'Palabra Clave:',
+                },
+            ]);
+            const cifradoCesar = new Cifrado(response.clave, response.mensajeEntrada, alfabeto);
+            console.log(`\nEl cifrado se realizará con el alfabeto: ${alfabeto}`);
+            console.log(`\nMensaje de entrada: ${response.mensajeEntrada}`);
+            console.log(`Palabra clave: ${response.clave}`);
+            console.log(`\nResultado del cifrado: ${cifradoCesar.encode()}`);
+            } else if(answers.eleccion == "Decodificar") {
+                const response = await prompt([
+                    {
+                        type: 'input',
+                        name: 'mensajeEntrada',
+                        message: 'Mensaje encriptado:',
+                    },
+                    {
+                        type: 'input',
+                        name: 'clave',
+                        message: 'Palabra Clave:',
+                    },
+                ]);
+                const cifradoCesar = new Cifrado(response.clave, response.mensajeEntrada, alfabeto);
+                console.log(`\nEl cifrado se realizará con el alfabeto: ${alfabeto}`);
+                console.log(`\nMensaje encriptado: ${response.mensajeEntrada}`);
+                console.log(`Palabra clave: ${response.clave}`);
+                console.log(`\nPalabra descifrada: ${cifradoCesar.decode()}`);
+            } else if (answers.eleccion == "Salir") {
+                stop = true;
+                return 0;
+            } else {
+                console.log('\nOpción no válida.');
+                return -1;
+            }
+          });
+    }
+};
 
 start();
